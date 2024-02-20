0a674368f5fcb156da2570dc7ee609ba90becf8c
Anna Prymšová <anca7@centrum.cz>
2024-02-20 21:57:28 +0100

    Done

--- src/main/java/cz/czechitas/ukol4/Sportka.java
+++ src/main/java/cz/czechitas/ukol4/Sportka.java
@@ -12,6 +12,10 @@ public class Sportka {
 
     public Sportka() {
         //TODO naplnit osudí čísly 1 až 49.
+        for (int i = 0; i < 49; i++) {
+            int cislo = i + 1;
+            osudi.add(cislo);
+        }
     }
 
     /**
@@ -21,6 +25,7 @@ public class Sportka {
      */
     public void zamichat() {
         //TODO zamíchat osudí
+       Collections.shuffle(osudi);
     }
 
     /**
@@ -31,7 +36,7 @@ public class Sportka {
      */
     public List<Integer> vylosovanaCisla() {
         //TODO Vrátit seznam prvních 6 čísel z osudí.
-        return null;
+        return osudi.subList(0, 6);
     }
 
     /**
@@ -41,6 +46,6 @@ public class Sportka {
      */
     public Integer dodatkoveCislo() {
         //TODO Vrátit sedmé číslo z osudí.
-        return null;
+        return osudi.get(6);
     }
 }
